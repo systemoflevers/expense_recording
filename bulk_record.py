ab4e324c07ae6f1b84b2084f73162308625c9854
Jesse Doherty <jewido@gmail.com>
2016-12-27 17:54:19 -0500

    forming query string

--- bulk_record.py
+++ bulk_record.py
@@ -1,22 +1,41 @@
-#!/usr/bin/env python
+#!/usr/bin/env python                                                                                                                                        
+                                                                                                                                                             
+import cgi                                                                                                                                                   
+import cgitb; cgitb.enable()                                                                                                                                 
+                                                                                                                                                             
+def headers():                                                                                                                                               
+  print "content-type: text/plain"                                                                                                                           
+  print                                                                                                                                                      
+         
 
-import cgi
-import cgitb; cgitb.enable()
-
-def headers():
-  print "content-type: text/plain"
-  print
+def getParameters(expense):
+  values = expense.split("|")
+  # WHEN|WHAT|AMOUNT|JOINT|DESCRIPTION
+  parameters = {
+    "who" : values[0],
+    "when" : values[1],
+    "what" : values[2],
+    "amount" : values[3],
+    "joint" : values[4],
+    "description"  values[5],
+  }
   
-
-def main():
-  form = cgi.FieldStorage()
-  expenses = form.getList("expense")
+  parameterList = (
+    "entry.272749610=%(who)s&entry.2037284352=%(what)s&entry.671256177=%(type)s"
+    "&entry.234466585=%(amount)s&entry.696825855=%(when)"
+    "&entry.2125473046=%(desc)s"
+  )
   
-  headers()
-  
-  for e in expenses:
-    print  e
-  print "done"
-
-if __name__ == "__main__":
-  main()
+  return parameterList % parameters
+                                                                                                                                                             
+def main():                                                                                                                                                  
+  form = cgi.FieldStorage()                                                                                                                                  
+  expenses = form.getlist("expense")                                                                                                                         
+                                                                                                                                                             
+  headers()                                                                                                                                                  
+                                                                                                                                                             
+  for e in expenses:                                                                                                                                         
+    print  getParameters(e)                                                                                                                                                 
+  print "done"                                                                                                                                               
+                                                                                                                                                             
+main()
